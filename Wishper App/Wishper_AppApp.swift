43c48b3ea5830d491c3571cfcd86b5d061d5c1bd
Ranga Reddy Nukala <itsmerr17@gmail.com>
2026-04-11 10:52:10 -0700

    Initial Commit

---
+++ Wishper App/Wishper_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wishper_AppApp.swift
+//  Wishper App
+//
+//  Created by Ranga Reddy Nukala on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Wishper_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wishper AppTests/Wishper_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wishper_AppTests.swift
+//  Wishper AppTests
+//
+//  Created by Ranga Reddy Nukala on 4/11/26.
+//
+
+import Testing
+@testable import Wishper_App
+
+struct Wishper_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
